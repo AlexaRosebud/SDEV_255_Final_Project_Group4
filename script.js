18375ffbbaa05ea6d6c7aed75d64d37dd99b67cf
Katharine Brumback <93019406+MissLadyPandas@users.noreply.github.com>
2023-02-17 07:40:45 +0000

    Added confirmation.html for form submission - created new form link

---
+++ script.js
@@ -0,0 +1,7 @@
+function confirmSubmit() {
+  var result = confirm("Are you sure you want to submit this form?");
+  if (result == true) {
+       document.getElementById("myForm").submit();
+  } else {
+   }
+}

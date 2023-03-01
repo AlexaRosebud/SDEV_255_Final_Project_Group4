7eb6de5285a5941aac31a0afa5250f3af1abaf2c
MissLadyPandas <kbrumback@ivytech.edu>
2023-02-28 20:15:24 -0500

    added mongo db - added create course page

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Course = require('./models/courses');
 
 //express app
 const app = express();
 
-// listen for requests
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
-
 //connect to mongodb
-// const dbURI ='mongodb+srv://kbrumback:test1234@cluster0.kpdgknd.mongodb.net/node-tuts?retryWrites=true&w=majority';
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
-//     .then((result) => app.listen(3000))
-//     .catch((err) => console.log(err));
+const dbURI ='mongodb+srv://kbrumback:test1234@cluster0.kpdgknd.mongodb.net/FinalProject?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log('Server started on port 3000');
+        });
+    })
+    .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -29,12 +28,17 @@ app.use(morgan('dev'));
 
 // home page route
 app.get('/', (req, res) => {
-    res.redirect('/index');
+    res.redirect('/home');
 });
 
-// index page route
-app.get('/index', (req, res) => {
-    res.render('index', { title: 'Home' });
+// home page route
+app.get('/home', (req, res) => {
+    res.render('home', { title: 'Home' });
+});
+
+// about page route
+app.get('/create', (req, res) => {
+    res.render('create', { title: 'Create Course' });
 });
 
 // about page route
@@ -69,8 +73,36 @@ app.get('/shop', (req, res) => {
 
 // courses route
 app.get('/courses', (req, res) => {
-    res.render('courses', { title: 'Courses' });
-});
+    Course.find()
+      .then(courses => {
+        res.render('courses', { title: 'Courses', courses });
+      })
+      .catch(err => console.log(err));
+  });
+  
+  
+
+// course details route
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    Course.findById(id)
+      .then(course => {
+        res.render('course_details', { title: 'Course Details', course });
+      })
+      .catch(err => console.log(err));
+  });
+  
+  app.post('/create-course', (req, res) => {
+    const { name, credits, description } = req.body;
+    const course = new Course({ name, credits, description });
+  
+    course.save()
+      .then(() => {
+        res.redirect('/courses');
+      })
+      .catch(err => console.log(err));
+  });
+  
 
 // 404 page - this HAS to be at the bottom of the page otherwise express will match based on per list item
 app.use((req, res) => {

462fb233fbe01abe720fc17ab6070eb85e49fac3
MissLadyPandas <kbrumback@ivytech.edu>
2023-02-28 21:47:27 -0500

    final changes

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Course = require('./models/courses');
 
+
 //express app
 const app = express();
+app.set('view engine', 'ejs');
 
 //connect to mongodb
 const dbURI ='mongodb+srv://kbrumback:test1234@cluster0.kpdgknd.mongodb.net/FinalProject?retryWrites=true&w=majority';
@@ -16,6 +18,23 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
     })
     .catch((err) => console.log(err));
 
+// mongoose and mongo sandbox routes
+const courseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  credits: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
+
+
 // register view engine
 app.set('view engine', 'ejs');
 
@@ -23,8 +42,6 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
-
-// mongoose and mongo sandbox routes
 
 // home page route
 app.get('/', (req, res) => {
@@ -71,38 +88,88 @@ app.get('/shop', (req, res) => {
     res.render('shop', { title: 'Shop' });
 });
 
+//new course route
+app.post('/courses', (req, res) => {
+  const { name, credits, description } = req.body;
+  const newCourse = new Course({ name, credits, description });
+  newCourse.save()
+    .then(() => {
+      res.redirect('/courses');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error creating course');
+    });
+});
+
+// create course route
+app.post('/create-course', (req, res) => {
+  const course = new Course({
+    name: req.body.name,
+    credits: req.body.credits,
+    description: req.body.description
+  });
+
+// save to database
+  course.save()
+    .then(result => {
+      res.json({redirect: '/create'});
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+
+// delete course route
+app.post('/courses/:id/delete', (req, res) => {
+  const courseId = req.params.id;
+  Course.findByIdAndDelete(courseId)
+    .then(() => {
+      res.redirect('/courses');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error deleting course');
+    });
+});
+
+
 // courses route
 app.get('/courses', (req, res) => {
-    Course.find()
-      .then(courses => {
-        res.render('courses', { title: 'Courses', courses });
-      })
-      .catch(err => console.log(err));
+    const courses = [
+      { name: 'Math', credits: 3 },
+      { name: 'Science', credits: 4 },
+      { name: 'English', credits: 2 }
+    ];
+    res.render('create', { title: 'Create Course', courses });
+});
+
+// Handle POST request to create a new course
+app.post('/create-course', (req, res) => {
+    const { name, credits, description } = req.body;
+      res.redirect('/courses');
   });
-  
   
 
-// course details route
-app.get('/courses/:id', (req, res) => {
-    const id = req.params.id;
-    Course.findById(id)
-      .then(course => {
-        res.render('course_details', { title: 'Course Details', course });
-      })
-      .catch(err => console.log(err));
-  });
+//courses find route
+// app.get('/courses', (req, res) => {
+//     Course.find()
+//       .then(courses => {
+//         res.render('courses', { title: 'Courses', courses });
+//       })
+//       .catch(err => console.log(err));
+//   });
   
-  app.post('/create-course', (req, res) => {
-    const { name, credits, description } = req.body;
-    const course = new Course({ name, credits, description });
-  
-    course.save()
-      .then(() => {
-        res.redirect('/courses');
-      })
-      .catch(err => console.log(err));
-  });
-  
+// // course details route
+// app.get('/courses/:id', (req, res) => {
+//     const id = req.params.id;
+//     Course.findById(id)
+//       .then(course => {
+//         res.render('course_details', { title: 'Course Details', course });
+//       })
+//       .catch(err => console.log(err));
+//   });
 
 // 404 page - this HAS to be at the bottom of the page otherwise express will match based on per list item
 app.use((req, res) => {
